0a3cacc9dc58fd80ab1aa9808c157a583dac1027
Gustavo Gantois <gugagantois@gmail.com>
2025-09-23 23:15:03 -0300

    style:Modify fonts for project.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,17 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Lato, Bodoni_Moda_SC } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const lato = Lato({
+  variable: "--font-lato",
   subsets: ["latin"],
+  weight: ["100", "300", "400", "700", "900"]
 });
 
-const geistMono = Geist_Mono({
+const bodoniModaSC = Bodoni_Moda_SC({
   variable: "--font-geist-mono",
   subsets: ["latin"],
+  weight: ["400", "500", "600", "700", "800", "900"]
 });
 
 export const metadata: Metadata = {
@@ -25,7 +27,7 @@ export default function RootLayout({
   return (
     <html lang="pt-br">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${lato.variable} ${bodoniModaSC.variable} antialiased`}
       >
         {children}
       </body>
