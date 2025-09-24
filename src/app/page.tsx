cfba80210bff6b3a4286f36c30477f113bc1d716
Gustavo Gantois <gugagantois@gmail.com>
2025-09-24 08:30:27 -0300

    feat:Create service section component.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import { HomeAbout } from "./s-about";
 import { HomeContact } from "./s-contact";
+import { HomeServices } from "./s-services";
 
 export default function Page() {
   return (
     <>
     <HomeAbout />
+    <HomeServices />
     <HomeContact />
     </>
   );
