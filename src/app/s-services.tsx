aca8c2df800ebf907f4f8d400ceadc40c1864ae9
Gustavo Gantois <gugagantois@gmail.com>
2025-09-24 08:51:29 -0300

    feat:Adding content in service section.

--- src/app/s-services.tsx
+++ src/app/s-services.tsx
@@ -1,6 +1,20 @@
+import { serviceData } from "./data/v-data";
+
 export function HomeServices() {
     return (
         <>
+        <section className="max-w-[1440px] mx-auto p-8 mt-10 lg:mt-[100px]" id="services">
+            {serviceData.map((item, index) => {
+                return (
+                    <div key={index}>
+                        <div className="flex flex-col justify-center items-center gap-4">
+                            <h1 className="text-black text-[50px] lg:text-[60px] text-center">{item.title}</h1>
+                            <p className="text-[#5A5A5A] text-base lg:text-xl text-center w-full lg:max-w-[951px]">{item.description}</p>
+                        </div>
+                    </div>
+                )
+            })}
+        </section>
         </>
     )
 }
