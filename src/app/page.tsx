f8412a8f15c1df2ef3294f27617cd43a062d5ffc
Gustavo Gantois <gugagantois@gmail.com>
2025-09-24 00:00:46 -0300

    feat:Create about section.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
+import { HomeAbout } from "./s-about";
 import { HomeContact } from "./s-contact";
 
 export default function Page() {
   return (
     <>
+    <HomeAbout />
     <HomeContact />
     </>
   );
--- src/app/s-about.tsx
+++ src/app/s-about.tsx
@@ -1,22 +1,24 @@
 import Link from "next/link";
 import { aboutData } from "./data/v-data";
-import { ArrowArcRight } from "phosphor-react";
+import { ArrowRight } from "lucide-react";
 
 export function HomeAbout() {
     return (
-        <>
-        <section className="max-w-[1440px] mx-auto p-8 mt-10 lg:mt-[100px]">
+        <section className="w-full border-t border-b border-gray-300 mt-10 lg:mt-[100px] p-8">
+        <div className="max-w-[1440px] mx-auto p-8">
             <div className="flex flex-col lg:flex-row justify-start items-start lg:justify-between gap-4">
                 <h1 className="text-black text-[50px] font-semibold lg:text-[60px]">{aboutData.title}</h1>
-                <p className="text-start text-[#5A5A5A] max-w-[509px] text-base lg:text-xl">{aboutData.description}</p>
-                <button>
-                    <Link className="flex items-center gap-1" href="#services" target="_blank">
+                <article className="flex flex-col justify-start gap-4">
+                    <p className="text-start text-[#5A5A5A] w-full lg:max-w-[509px] text-base lg:text-xl">{aboutData.description}</p>
+                <button className="underline w-full transition-all duration-1000 hover:translate-x-4">
+                    <Link className="flex items-center gap-2 text-black" href="#services" target="_blank">
                         {aboutData.btnText}
-                        <ArrowArcRight className="text-black rotate-12" size={24} />
+                        <ArrowRight className="text-black -rotate-12" size={16} />
                     </Link>
                 </button>
+                </article>
             </div>
+        </div>
         </section>
-        </>
     )
 }
