61ecf33f6ab4b3c4056579f60ffdb6be8c526dfc
Gustavo Gantois <gugagantois@gmail.com>
2025-09-25 08:37:41 -0300

    feat:Create component Hero section.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import { HomeAbout } from "./s-about";
 import { HomeContact } from "./s-contact";
 import { HomeFAQ } from "./s-faq";
+import { HomeHero } from "./s-hero";
 import { HomeServices } from "./s-services";
 
 export default function Page() {
   return (
     <>
+    <HomeHero />
     <HomeAbout />
     <HomeServices />
     <HomeFAQ />
