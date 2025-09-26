813a6e78f577a374d5dbcd990212f1a0885ca18f
Gustavo Gantois <gugagantois@gmail.com>
2025-09-26 08:36:01 -0300

    fix:Fix errors in library AOS Animation.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,8 +6,20 @@ import { HomeContact } from "./s-contact";
 import { HomeFAQ } from "./s-faq";
 import { HomeHero } from "./s-hero";
 import { HomeServices } from "./s-services";
+import { useEffect } from "react";
+//library
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 export default function Page() {
+
+  useEffect(() => {
+    AOS.init({
+    duration: 900,
+    once: false,
+  })
+  }, [])
+
   return (
     <>
     <HomeHero />
