24780cb58ef37152040f52a49168ffe81539dd4c
Gustavo Gantois <gugagantois@gmail.com>
2025-09-26 00:24:08 -0300

    style:Modify styles of button in hero section.

--- src/app/s-hero.tsx
+++ src/app/s-hero.tsx
@@ -36,7 +36,7 @@ export function HomeHero() {
                                     </button>
                                 </Link>
                                 <Link href={heroData.hrefProject}>
-                                    <button className="bg-[#FFFFFF] cursor-pointer text-black text-base lg:text-xl transition-all duration-700 hover:-translate-y-2 py-2 px-[28px]">
+                                    <button className="bg-white cursor-pointer z-50 text-black underline text-base lg:text-xl transition-all duration-700 hover:-translate-y-2 py-2 px-[28px]">
                                         {heroData.btnProject}
                                     </button>
                                 </Link>
