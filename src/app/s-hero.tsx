8496cea09d17c7a89777f2215b27bb032474d77b
Gustavo Gantois <gugagantois@gmail.com>
2025-09-26 00:18:17 -0300

    style:Adding scroll-behavior smooth.

--- src/app/s-hero.tsx
+++ src/app/s-hero.tsx
@@ -35,7 +35,7 @@ export function HomeHero() {
                                         {heroData.btnText}
                                     </button>
                                 </Link>
-                                <Link href={heroData.hrefProject}>
+                                <Link href={heroData.hrefProject} className="bg-white">
                                     <button className="bg-white cursor-pointer text-black text-base lg:text-xl transition-all duration-700 hover:-translate-y-2 py-2 px-[28px]">
                                         {heroData.btnProject}
                                     </button>
