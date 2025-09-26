597de431d62be1272377ca0625478013a427cbac
Gustavo Gantois <gugagantois@gmail.com>
2025-09-26 00:19:07 -0300

    style:Fix styles in button.

--- src/app/s-hero.tsx
+++ src/app/s-hero.tsx
@@ -35,8 +35,8 @@ export function HomeHero() {
                                         {heroData.btnText}
                                     </button>
                                 </Link>
-                                <Link href={heroData.hrefProject} className="bg-white">
-                                    <button className="bg-white cursor-pointer text-black text-base lg:text-xl transition-all duration-700 hover:-translate-y-2 py-2 px-[28px]">
+                                <Link href={heroData.hrefProject}>
+                                    <button className="bg-[#FFFFFF] cursor-pointer text-black text-base lg:text-xl transition-all duration-700 hover:-translate-y-2 py-2 px-[28px]">
                                         {heroData.btnProject}
                                     </button>
                                 </Link>
